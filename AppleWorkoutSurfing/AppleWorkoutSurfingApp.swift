2b7aec367898b036f6d32173558b138d6f74ddff
Renato Martins <rochamartinsrenato@gmail.com>
2023-10-19 08:08:02 +0100

    Initial Commit

---
+++ AppleWorkoutSurfing/AppleWorkoutSurfingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleWorkoutSurfingApp.swift
+//  AppleWorkoutSurfing
+//
+//  Created by Renato Martins on 19/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleWorkoutSurfingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
